636511633748574a05fb244e91de820c5a113fe6
Xavier Twitty <x.twitty@gmail.com>
2021-07-29 19:39:01 -0400

    wip

--- api/auth/auth_router.js
+++ api/auth/auth_router.js
@@ -1,15 +1,45 @@
 const router = require("express").Router()
 const {insertUser} = require("../users/users_model")
 const {validateUser, validateRoleNAme, checkIfUsernameExists} = require("./auth_middleware")
+const {jwtSecret} =require("../secrets")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
-router.post('/register', validateUser, validateRoleNAme, async (req, res) => {
-    
-  // res.status(201).json(await insertUser(req.body))
+
+router.post('/register', validateUser, validateRoleNAme, async (req, res, next) => {
+    const {username , password, email} = req.body
+    const hash = bcrypt.hashSync(password, 8)
+
+  res.status(201).json(await insertUser({username, password: hash, email}))
+  next()
   })
 
   router.post('/login', checkIfUsernameExists, (req, res, next) => {
-    res.json({message: "logged in "})
+    if (bcrypt.compareSync(req.body.username, req.body.password)) {
+      const token = buildToken(req.user)
+      res.status(200).json({
+        message: `${req.user.username} Welcome Back!`,
+        token,
+      })
+    } else {
+      next({
+        status: 400,
+        message: "invalid credentials"
+      })
+    }
       
   })
+
+  function buildToken(user) {
+    const payload = {
+      subject: user.user_id,
+      email: user.email,
+      username: user.username
+    }
+    const options = {
+      expiresIn: "id"
+    }
+    return jwt.sign(payload, jwtSecret, options)
+  }
 
   module.exports = router
--- api/data/seeds/03_classes.js
+++ api/data/seeds/03_classes.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('classes').insert([
-        {class_name: 'Cross Country', class_type:"Running", startTime:"8am", startDate: "3/1/21", location: "Miami Gardens", duration: 1 , intensity: 10, user_id : 2},
-        {class_name: 'Fitness Time', class_type:"Aerobics", startTime:"12am", startDate: "3/2/21", location: "Clearwater", duration: 2 , intensity: 3 , user_id: 1},
-        {class_name: 'Dance workout', class_type:"Zumba", startTime:"2pm", startDate: "3/3/21", location: "Tampa", duration: 2, intensity: 7, user_id: 3},
-        {class_name: 'Muscle Building', class_type:"Weight Lifting", startTime:"3pm", startDate: "3/4/21", location: "Miami", duration: 2, intensity: 7, user_id: 2}
+        {class_name: 'Cross Country', class_type:"Running", startTime:"08:00AM", startDate: "03-1-21", location: "Miami Gardens", duration: 1 , intensity: 10, user_id : 2},
+        {class_name: 'Fitness Time', class_type:"Aerobics", startTime:"08:00AM", startDate: "03-2-21", location: "Clearwater", duration: 2 , intensity: 3 , user_id: 1},
+        {class_name: 'Dance workout', class_type:"Zumba", startTime:"08:00AM", startDate: "03-3-21", location: "Tampa", duration: 2, intensity: 7, user_id: 3},
+        {class_name: 'Muscle Building', class_type:"Weight Lifting", startTime:"08:00AM", startDate: "03/4/21", location: "Miami", duration: 2, intensity: 7, user_id: 2}
       ]); // error with decimals in duration 
     });
 };
